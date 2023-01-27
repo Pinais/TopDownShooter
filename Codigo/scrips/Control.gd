333f133daf2902c9c6d4f7c4911df7eaed363635
Lucas Sales Salvo Petruci <99514230+LucasPetruci@users.noreply.github.com>
2023-01-26 23:37:13 -0300

    Delete Codigo directory

--- Codigo/scrips/Control.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-var tamanho_vidas = 20
-
-func on_mudar_vidas_jogador(vidas_jogador) -> void:
-	$vidas.rect_size.x = vidas_jogador * tamanho_vidas
--- Codigo/scrips/Inimigo_simples.gd
+++
@@ -1,6 +0,0 @@
-extends "res://scrips/base_inimigo.gd"
-
-
-func _process(delta):
-	movimento_basico_inimigo(delta)
-	
