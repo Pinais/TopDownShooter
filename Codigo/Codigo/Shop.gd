3ea425cf8c9fd41383d1aaca8bf60b69d7459e9c
João Victor Capello <43796065+Pinais@users.noreply.github.com>
2023-02-20 20:45:58 -0300

    Dia 11 de Desenvolvimento - Corrigindo a funcionalidade da Loja
    Agora a loja gera 4 itens aleatórios toda vez que é atualizada, deletando itens anteriores ao invés de mantê-los no mapa.
    Isso não quer dizer que a funcionalidade esteja 100% pronta, porém os erros presentes no dia 10 estão corrigidos.

--- Codigo/Codigo/Shop.gd
+++ Codigo/Codigo/Shop.gd
@@ -2,75 +2,49 @@ extends Node2D
 
 
 export (Array, PackedScene) var lista_possiveis_itens
+export (Array, Vector2) var lista_posicao_item
 
 
 var item1
 var item2
 var item3
 var item4
-var botao_ativo := false
+var lista_itens := [item1, item2, item3, item4]
+var botao_ativo : bool = false
 
 
 onready var botao_reroll := $BotaoReroll
-onready var posicao_item1 := $PosicaoItem1
-onready var posicao_item2 := $PosicaoItem2
-onready var posicao_item3 := $PosicaoItem3
-onready var posicao_item4 := $PosicaoItem4
+onready var gerente_itens := $GerenteItens
 
 
 func _ready():
-	gerar_itens_aleatorios(true,true,true,true)
+	trocar_itens()
 
 
-func gerar_itens_aleatorios(pos1:bool, pos2:bool, pos3:bool, pos4:bool):
-	print("ok")
-	var lista_reroll_atual = lista_possiveis_itens.duplicate(true)
-	if pos1:
-		var tamanho_lista_itens = len(lista_possiveis_itens)
-		var valor_aleatorio = int(rand_range(0, tamanho_lista_itens))
-		var item_escolhido = lista_possiveis_itens[valor_aleatorio]
-		lista_reroll_atual.erase(item_escolhido)
-		item1 = item_escolhido.instance()
-		item1.position = posicao_item1.position
-		item1.scale = Vector2(5,5)
-		add_child(item1)
+func trocar_itens():
+	var qtd_itens = len(lista_itens)
+	var i = 0
+	while(i < qtd_itens):
+		if lista_itens[i] != null and is_instance_valid(lista_itens[i]):
+			lista_itens[i].queue_free()
+			gerente_itens.remove_child(lista_itens[i])
 		
-	if pos2:
-		var tamanho_lista_itens = len(lista_possiveis_itens)
-		var item_escolhido = lista_possiveis_itens[int(rand_range(0, tamanho_lista_itens))]
-		lista_reroll_atual.erase(item_escolhido)
-		item2 = item_escolhido.instance()
-		item2.position = posicao_item2.position
-		item2.scale = Vector2(5,5)
-		add_child(item2)
+		var item_escolhido = lista_possiveis_itens[int(rand_range(0, len(lista_possiveis_itens)))]
 		
-	if pos3:
-		var tamanho_lista_itens = len(lista_possiveis_itens)
-		var item_escolhido = lista_possiveis_itens[int(rand_range(0, tamanho_lista_itens))]
-		lista_reroll_atual.erase(item_escolhido)
-		item3 = item_escolhido.instance()
-		item3.position = posicao_item3.position
-		item3.scale = Vector2(5,5)
-		add_child(item3)
-		
-	if pos4:
-		var tamanho_lista_itens = len(lista_possiveis_itens)
-		var item_escolhido = lista_possiveis_itens[int(rand_range(0, tamanho_lista_itens))]
-		lista_reroll_atual.erase(item_escolhido)
-		item4 = item_escolhido.instance()
-		item4.position = posicao_item4.position
-		item4.scale = Vector2(5,5)
-		add_child(item4)
+		lista_itens[i] = item_escolhido.instance()
+		gerente_itens.add_child(lista_itens[i])
+		lista_itens[i].position = lista_posicao_item[i]
+		lista_itens[i].scale = Vector2(5,5)
+		i += 1
+
 
 func _unhandled_input(event):
 	if event.is_action_pressed("interagir") and botao_ativo:
-			gerar_itens_aleatorios(true,true,true,true)
+		trocar_itens()
+		pass
 
-func _on_BotaoReroll_area_entered(area):
-	print("ta dentro")
-	gerar_itens_aleatorios(true,true,true,true)
+func _on_BotaoReroll_area_entered(_area):
 	botao_ativo = true
 	
-func _on_BotaoReroll_area_exited(area):
-	print("ta fora")
+func _on_BotaoReroll_area_exited(_area):
 	botao_ativo = false
